c8bfde786785408b5ab87d0d415646dd132852b3
SHYAKA Aime <shyakaaime25@gmail.com>
2024-04-19 14:37:29 +0200

    updated loading images

--- src/controllers/blogController.ts
+++ src/controllers/blogController.ts
@@ -4,6 +4,14 @@ import User from '../models/user';
 import { createBlogSchema, updateBlogSchema } from '../utils/validation';
 import multer from 'multer';
 import path from 'path';
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'di67gv9fp', 
+  api_key: '774151655517525', 
+  api_secret: 'dyru31EX2OWR0PMD8P20NDW06iQ' 
+});
+
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -31,7 +39,8 @@ export const createBlog = async (req: Request, res: Response): Promise<void> => 
       }
 
       const { title, description } = req.body;
-      const image = req.file?.filename; // Use optional chaining to safely access filename
+      const imageUploadResult = await cloudinary.uploader.upload(req.file?.path as string); // Upload image to Cloudinary
+      const image = imageUploadResult.public_id; // Use the public_id property as the image identifier
       const blog = new Blog({ title, description, image });
       await blog.save();
       res.status(201).json({ message: 'Blog Created successfully', blog });
@@ -83,20 +92,26 @@ export const updateBlog = async (req: Request, res: Response): Promise<void> => 
       }
 
       const { title, description } = req.body;
-      const image = req.file?.filename; // Use optional chaining to safely access filename
+      let image;
+      if (req.file) {
+        const imageUploadResult = await cloudinary.uploader.upload(req.file.path);
+        image = imageUploadResult.public_id;
+      }
+
       const blog = await Blog.findByIdAndUpdate(id, { title, description, image }, { new: true });
       if (!blog) {
         res.status(404).json({ message: 'Blog not found' });
         return;
       }
 
-      res.status(200).json({ message: 'Blog Updated successfully', blog});
+      res.status(200).json({ message: 'Blog Updated successfully', blog });
     });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
   }
 };
+
 
 
 export const deleteBlog = async (req: Request, res: Response): Promise<void> => {
